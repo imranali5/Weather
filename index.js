c2942fd5afed2b7edf94df4134509dcf3ac23b7b
imranali5 <156561048+imranali5@users.noreply.github.com>
2025-03-18 02:02:34 +0530

    Update index.js( add Auto geometric location update)

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ async function checkWeather(city) {
     document.querySelector(".city").innerHTML=data.name;
     document.querySelector(".temp").innerHTML=data.main.temp+" °C";
     document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
-    document.querySelector(".wind").innerHTML=data.wind.speed+" km/h";
+    document.querySelector(".wind").innerHTML=Math.round(data.wind.speed*3.6*100)/100+" km/h";
     document.querySelector(".feel").innerHTML=data.main.feels_like+"°C";
     document.querySelector(".wind_deg").innerHTML=data.wind.deg+"°";
     if(data.weather[0].main=="Clouds"){
@@ -39,6 +39,61 @@ async function checkWeather(city) {
     
     
 }
+
 searchbtn.addEventListener("click",()=>{
       checkWeather(searchbox.value);
 })
+
+
+// ----------------------------------------------------------------------------------
+
+
+
+
+ async function gotlocation(position){
+    console.log(position);
+    
+   let lat=position.coords.latitude;
+   let lon=position.coords.longitude;
+   console.log(lat);
+   console.log(lon);
+
+ const res= await fetch(apiurl+`&lat=${lat}&lon=${lon}&appid=${apikey}`);
+    var data=await res.json();
+
+    console.log(data);
+    document.querySelector(".city").innerHTML=data.name;
+    document.querySelector(".temp").innerHTML=data.main.temp+" °C";
+    document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+    document.querySelector(".wind").innerHTML=Math.round(data.wind.speed*3.6*100)/100+" km/h";
+    document.querySelector(".feel").innerHTML=data.main.feels_like+"°C";
+    document.querySelector(".wind_deg").innerHTML=data.wind.deg+"°";
+    if(data.weather[0].main=="Clouds"){
+        weathericon.src="./image/cloudy.png";
+    }
+    else if(data.weather[0].main=="Clear"){
+        weathericon.src="./image/sun.png";
+    }
+    else if(data.weather[0].main=="Rain"){
+        weathericon.src="./image/rain.png";
+    }
+    else if(data.weather[0].main=="Drizzle"){
+        weathericon.src="./image/drizzle.png";
+    }
+    else if(data.weather[0].main=="Mist"){
+        weathericon.src="./image/mist.png";
+    }
+    else if(data.weather[0].main=="Haze"){
+        weathericon.src="./image/haze.png";
+    }
+    else if(data.weather[0].main=="Snow"){
+        weathericon.src="./image/snow.png";
+    }
+}
+function faildtoget(){
+    console.log("sorry! we did not face your location");
+    
+}
+navigator.geolocation.getCurrentPosition(gotlocation,faildtoget);
+
+
